bc179d5cc99133ed78ded8eefe9d4593c156371b
stortor <storrtor@gmail.com>
2021-11-14 14:03:39 +0300

    Initial commit

---
+++ src/test/java/com/stortor/webhomework3/WebHomework3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stortor.webhomework3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebHomework3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
